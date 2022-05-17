1fac66a96c09ad893f714e1013e20de80de12957
Trần Dũng <93395558+TranDung6129@users.noreply.github.com>
2022-05-17 17:01:10 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Nhập dữ liệu đầu vào
+
+# Trích xuất các thông tin cần thiết từ dữ liệu
+
+# Chạy thuật toán để tìm ra các phương án chấp nhận được
+
+# Chọn phương án phù hợp nhất với yêu cầu
+
+# Tạo giao diện người dùng 
+
+# Xuất ra file với phương án phù hợp đã chọn
