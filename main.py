18c415b94aa628736c9f4da73e533f6890a163a4
Trần Dũng <93395558+TranDung6129@users.noreply.github.com>
2022-05-21 08:30:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 # Nhập dữ liệu đầu vào
+import os
+import pandas as pd
+import numpy as np
+from classroom import Classroom
+from information import ClassInformation
 
+'''Nhập đường dẫn và cột không tên là em chưa nghĩ ra hướng giải quyết phù hợp, 
+em dự định sử dụng file nhưng thử mãi chưa dùng được nên chị có thể kiểm tra giúp
+em xem có phù hợp không nếu không thì sẽ nghĩ cách khác ạ'''
+'''Ở trên là cách giải quyết đường dẫn như nhập file Excel nhưng còn cột không được 
+đặt tên thì em vẫn chưa nghĩ ra cách giải quyết ạ'''
+classroom = Classroom()
+information = ClassInformation()
+
+print(information.get_table())
 # Trích xuất các thông tin cần thiết từ dữ liệu
 
 # Chạy thuật toán để tìm ra các phương án chấp nhận được
