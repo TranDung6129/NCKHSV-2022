04788dee4eff41bfb6650287e4822edbfe127856
Trần Dũng <93395558+TranDung6129@users.noreply.github.com>
2022-05-28 01:44:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ em xem có phù hợp không nếu không thì sẽ nghĩ cách khác ạ'''
 milp_model = Model(name="milp")
 variable = []
 
-# classroom = Classroom()
+classroom = Classroom()
+classroom_df = classroom.df
 information = ClassInformation()
 information_df = information.df
-
+'''Trích xuất các thông tin cần thiết của mô hình'''
 # Tập A gồm n mã lớp sẽ mở trong kỳ xếp thời khóa biểu 
 A_set = [] 
 for class_code in information.get_class_code():
@@ -45,11 +46,27 @@ for class_code in A_set:
     class_periods = information.get_class_periods_number(class_code)
     H_set.append(class_periods)
 
-print(H_set)
-
 # Tập B là m mã phòng hiện có ở tòa D7 
-B = []
+B_set = []
+for classroom_name in classroom.get_class_room_list():
+    B_set.append(classroom_name)
 
+# Tập D là sức chứa của các phòng tương ứng với mã phòng:
+D_set = []
+for classroom_name in B_set:
+    D_set.append(classroom.get_classroom_capacity(classroom_name))
+
+
+# =============================================================================
+# Nhập ma trận G là ma trận biểu thị các lớp chia theo chương trình đào tạo.
+# Hàng của ma trận ứng với các lớp trong chương trình đào tạo
+# Cột của ma trận ứng với các mã lớp mở trong kỳ
+# Khởi tạo ma trận chứa tất cả các ô là 0, lấy số cột của ma trận và số hàng của
+# ma trận
+
+
+# =============================================================================
+'''Tạo ra các biến của mô hình'''
 # Tạo biến nhị phân x_m, thể hiện rằng phòng thứ m có được sử dụng hay không
 
 # Tạo biến nhị phân y_nm, thể hiện rằng mã lớp n được xếp vào phòng thứ m 
@@ -58,23 +75,20 @@ B = []
 
 # Tạo biến a_nti nếu mã lớp thứ n bắt đầu từ tiết thứ i của buổi t. i chạy trong tập 
 # {1, 2,..., 10} 
-    
+'''Đưa vào các hàm mục tiêu của mô hình'''
+# Số phòng được sử dụng ít nhất
 
-# =============================================================================
-# Nhập ma trận G là ma trận biểu thị các lớp chia theo chương trình đào tạo.
-# Hàng của ma trận ứng với các lớp trong chương trình đào tạo
-# Cột của ma trận ứng với các mã lớp mở trong kỳ
-# Khởi tạo ma trận chứa tất cả các ô là 0, lấy số cột của ma trận và số hàng của
-# ma trận
-list_of_class_code = []
-# =============================================================================
+# Số buổi có tiết học trong tuần của một lớp chia theo chương trình đào tạo là ít nhất
 
-class_code_in_matrix = []
-class_name_in_matrix = []
-# Chạy thuật toán để tìm ra các phương án chấp nhận được
+'''Đưa vào các ràng buộc của mô hình''' 
+# Mỗi mã lớp chỉ được xếp vào một phòng duy nhất
 
-# Chọn phương án phù hợp nhất với yêu cầu
+# Mỗi mã lớp chỉ được xếp vào một buổi học (thời gian bắt đầu và kết thúc phải cùng 
+# một buổi)
 
-# Tạo giao diện người dùng 
+# Mỗi mã lớp chỉ xếp vào buổi học duy nhất
 
-# Xuất ra file với phương án phù hợp đã chọn
+# Sức chứa của phòng học lớn hown sĩ số của mã lớp được xếp vào phòng đó
+
+# Lịch học của các mã lớp con không được trùng nhau
+
