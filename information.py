1027273bdfae310a68d0cdcd1a9f321760d9a852
Trần Dũng <93395558+TranDung6129@users.noreply.github.com>
2022-05-28 00:01:26 +0700

    Update information.py

--- information.py
+++ information.py
@@ -42,28 +42,27 @@ class ClassInformation:
             new_class_code.append(new_code)
         self.df.insert(0, "Mã lớp", new_class_code, True)
             
-        
     def get_table(self):
         '''In ra màn hình bảng chứa thông tin'''
         return self.df
     
     def get_student_number(self, class_code):
         ''' Trả về số sinh viên từng mã lớp.
-        class_code = ('Mã lớp của lớp cần lấy số sinh viên')'''
-        self.df.set_index("Mã lớp", inplace = True)
-        return self.df.loc[class_code]["Số SV lớp cố định"]
-    
+        class_code = ('Mã lớp')'''
+        class_index = self.df.index[self.df['Mã lớp'] == class_code].astype(int)[0]
+        return self.df.at[class_index, 'Số SV lớp cố định']
+
     def get_class_periods_number(self, class_code):
         '''Trả về số tiết học của một mã lớp trong kỳ, dựa vào khối lượng'''
-        self.df.set_index("Mã lớp", inplace = True)
-        periods_retrieve = self.df.loc[class_code]["KHỐI LƯỢNG "]
+        period_index = self.df.index[self.df['Mã lớp'] == class_code].astype(int)[0]
+        periods_retrieve = self.df.at[period_index, 'KHỐI LƯỢNG ']
         periods = periods_retrieve[0]
         return periods
     
     def get_participant_class(self, class_code):
-        '''Trả về tên của các lớp tham gia một mã lớp'''
-        self.df.set_index("Mã lớp", inplace = True)
-        participant_class = self.df.loc[class_code]["Lớp"]
+        '''Trả về tên của các lớp con tham gia một mã lớp'''
+        participant_class_index  = self.df.index[self.df['Mã lớp'] == class_code].astype(int)[0]
+        participant_class = self.df.at[participant_class_index, "Lớp"]
         participant_class_list = list(participant_class.split("+"))
         for i in participant_class_list:
             if i == 'B':
@@ -71,3 +70,7 @@ class ClassInformation:
                 participant_class_list[B_index - 1] = participant_class_list[B_index - 1] + "+" + participant_class_list[B_index]
                 participant_class_list.remove("B")
         return participant_class_list
+    
+    def get_class_code(self):
+        class_code = self.df["Mã lớp"].to_list()
+        return class_code
